d70e29229c8692b49f9a003f46e55a6fb8d7cddb
taktojakuba <112001303+taktojakuba@users.noreply.github.com>
2026-09-22 19:10:15 +0200

    remove coloring

--- main.cpp
+++ main.cpp
@@ -2,7 +2,6 @@
 #include <fstream>
 #include <string>
 #include <array>
-#include <cstdlib>
 
 #include "modules/hostname.h"
 #include "modules/osname.h"
@@ -12,72 +11,38 @@
 #include "modules/term.h"
 #include "modules/uptime.h"
 #include "modules/packages.h"
-
 using namespace std;
-
-string get_color_code(bool ascii)
-{
-  bool dark = true;
-  const char* colorfgbg = getenv("COLORFGBG");
-  if (colorfgbg)
-  {
-    string cfbg(colorfgbg);
-    size_t last_sep = cfbg.find_last_of(";");
-    if (last_sep != string::npos)
-    {
-      string bg = cfbg.substr(last_sep + 1);
-      if (bg != "0" && bg != "1" && bg != "2" && bg != "3" && bg != "4")
-        dark = false;
-    }
-  }
-
-  if (dark)
-    return ascii ? "\033[1;36m" : "\033[1;33m";
-  else
-    return ascii ? "\033[34m" : "\033[35m";
-}
 
 int main(int argc, char* argv[])
 {
-  const string color = get_color_code(true);
-  const string label_color = get_color_code(false);
-  const string reset = "\033[0m";
-
-  array<string, 8> labels = {
-    "Host", "OS", "Kernel", "Shell", "Term", "WM", "Uptime", "Packages"
+  array<string, 8> info = {
+    "Host: " + get_hostname(),
+    "OS: " + get_osname(),
+    "Kernel: " + get_kernel(),
+    "Shell: " + get_shell(),
+    "Term: " + get_term(),
+    "WM: " + get_wm(),
+    "Uptime: " + get_uptime(),
+    "Packages: " + get_packages()
   };
-  array<string, 8> values = {
-    get_hostname(),
-    get_osname(),
-    get_kernel(),
-    get_shell(),
-    get_term(),
-    get_wm(),
-    get_uptime(),
-    get_packages()
-  };
-
-  if (argc < 2)
+  if (argc < 2) 
   {
-    for (int i = 0; i < labels.size(); i++)
-    {
-      cout << label_color << labels[i] << reset << ": " << values[i] << endl;
+    for(int i = 0;i<info.size();i++){
+      cout << info[i] << endl;
     }
     return 0;
   }
-
+  //get file content lines
   ifstream file(argv[1]);
   string line;
   size_t i = 0;
-  while (getline(file, line))
-  {
-    cout << color << line << reset;
-    if (i < values.size())
-    {
-      cout << "  " << label_color << labels[i] << reset << ": " << values[i];
+  while (getline(file, line)) {
+    cout << line;
+    if (i < info.size()) {
+        cout << "  " << info[i];
     }
     cout << endl;
     ++i;
-  }
+}
   return 0;
 }
